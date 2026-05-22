07aa4c3c9406746b327b1ec5a2f008a9816ac4ab
Maria Elsha Thomas <mariaelshathomas@gmail.com>
2026-05-22 15:21:28 +0400

    initial commit

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,85 @@
+﻿using Employee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Employee.Controllers
+{
+    public class EmpController : Controller
+    {
+        public ActionResult Index()
+        {
+            Models.Emp_DAL emp = new Models.Emp_DAL();
+            return View(emp.Get_Details());
+        }
+        public ActionResult User_Index()
+        {
+            Models.Emp_DAL emp = new Models.Emp_DAL();
+            return View(emp.Get_Details());
+        }
+        public ActionResult Details(int id)
+        {
+            Employee.Models.Emp_DAL empl = new Employee.Models.Emp_DAL();
+            return View(empl.Get_Emp(id));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee.Models.Emp_Data emp)
+        {
+            try
+            {
+                Employee.Models.Emp_DAL created = new Employee.Models.Emp_DAL();
+                created.Create_Emp(emp);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            Employee.Models.Emp_DAL emp = new Employee.Models.Emp_DAL();
+            return View(emp.Get_Emp(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Employee.Models.Emp_Data emp)
+        {
+            try
+            {
+                Employee.Models.Emp_DAL emp1 = new Employee.Models.Emp_DAL();
+                emp1.Update_Emp(emp);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            Employee.Models.Emp_DAL emp = new Employee.Models.Emp_DAL();
+            Employee.Models.Emp_Data empdel = emp.Get_Emp(id);
+            return View(empdel);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, Employee.Models.Emp_Data emp)
+        {
+            try
+            {
+                Employee.Models.Emp_DAL emp1 = new Employee.Models.Emp_DAL();
+                emp1.Delete_Emp(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Employee.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Employee.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            Employee.Models.Login login = new Employee.Models.Login();
+            return View(login);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignIn(Employee.Models.Login login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            Employee.Models.Login_DAL log = new Employee.Models.Login_DAL();
+            bool isValid = log.Check(login.username, login.password);
+
+            if (isValid)
+            {
+                string Type = log.Verify(login.username, login.password);
+
+                if (login.username == "ADMIN" && login.password == "AdminPass123!" && Type == "admin")
+                {
+                    return RedirectToAction("Index", "Emp");
+                }
+                else
+                {
+                    return RedirectToAction("User_Index", "Emp");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username or password");
+                return View(login);
+            }
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(Employee.Models.Register reg)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(reg);
+            }
+            else
+            {
+                try
+                {
+                    Employee.Models.Login_DAL created = new Employee.Models.Login_DAL();
+                    created.Create_User(reg);
+                    return RedirectToAction("SignIn");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("",ex.Message);
+                    return View(reg);
+                }
+            }
+        }
+    }
+}
